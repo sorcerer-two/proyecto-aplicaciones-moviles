031775474951e76bebf2b40a5b25f14dbad841bf
sorcerer-two <fgonzalez9411@gmail.com>
2024-12-10 00:01:41 -0300

    Agregado datos de prueba para el login

--- app/src/main/java/com/example/proyecto_aplicaciones_moviles/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_aplicaciones_moviles/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.proyecto_aplicaciones_moviles
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -28,10 +30,20 @@ class MainActivity : AppCompatActivity() {
 
     fun setListeners(){
         //Hace que aparezca el layout de la tienda y desaparezca el login
-        //TODO: POR HACER - VERIFICAR DATOS DE LOGIN
+        //TODO: POR HACER - VERIFICAR DATOS DE LOGIN CON BASE DE DATOS
+        //DATOS DE PRUEBA
+        var testUser = "prueba1"
+        var testPass = "pass1"
+        //DATOS DE PRUEBA
         findViewById<Button>(R.id.btnLogin).setOnClickListener{
-            findViewById<RelativeLayout>(R.id.storeLayout).visibility = RelativeLayout.VISIBLE
-            findViewById<LinearLayout>(R.id.loginLayout).visibility = LinearLayout.GONE
+            var user = findViewById<EditText>(R.id.etUsername).text.toString()
+            var password = findViewById<EditText>(R.id.etPassword).text.toString()
+            if (testUser == user && testPass == password) {
+                findViewById<RelativeLayout>(R.id.storeLayout).visibility = RelativeLayout.VISIBLE
+                findViewById<LinearLayout>(R.id.loginLayout).visibility = LinearLayout.GONE
+            } else {
+                findViewById<TextView>(R.id.loginError).visibility = TextView.VISIBLE
+            }
         }
     }
 }
